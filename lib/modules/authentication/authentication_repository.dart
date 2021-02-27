5af4ec45d25e04118d5312f8b934ca20b06630e9
Gabriel <falis.gabriel@gmail.com>
2021-02-27 23:19:40 +0100

    Firebase Authentication Module By Bloc Library

---
+++ lib/modules/authentication/authentication_repository.dart
@@ -0,0 +1,4 @@
+library authentication_repository;
+
+export './repositories/AuthenticationRepository.dart';
+export './models/models.dart';
---
+++ lib/modules/authentication/models/models.dart
@@ -0,0 +1,6 @@
+export 'user/User.dart';
+
+//Validations
+export 'validation/Email.dart';
+export 'validation/Password.dart';
+export 'validation/ConfirmedPassword.dart';
